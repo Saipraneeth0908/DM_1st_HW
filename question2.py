f764023dd82e7c44023b745b849b6ec9d645f51f
Saipraneeth0908 <156871819+Saipraneeth0908@users.noreply.github.com>
2024-01-28 17:08:31 -0500

    Add files via upload

---
+++ question2.py
@@ -0,0 +1,19 @@
+def question2_1():
+    answer = ['Discrete', 'Ratio']
+    return answer
+
+def question2_2():
+    answer = ['Continuous', 'Ratio']
+    return answer
+
+def question2_3():
+    answer = ['Continuous', 'Interval']
+    return answer 
+
+def question2_4():
+    answer = ['Discrete', 'Ordinal']
+    return answer
+
+def question2_5():
+    answer = ['Nominal', 'Nominal']
+    return answer
