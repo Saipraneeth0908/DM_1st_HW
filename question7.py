693454c540ff854425b60afd764c9bc67a109c95
Saipraneeth0908 <156871819+Saipraneeth0908@users.noreply.github.com>
2024-01-28 22:53:16 -0500

    Add files via upload

---
+++ question7.py
@@ -0,0 +1,11 @@
+def question7_1():
+    answer = 'non-decreasing'
+    return answer
+
+def question7_2():
+    answer = 'increase/decrease'
+    return answer
+
+def question7_3():
+    answer = [(0, 2), (3, 4), (5, 'infinity')]
+    return answer
