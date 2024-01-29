e119c9cec6e8e7789a001d868728474a49eb589a
Saipraneeth0908 <156871819+Saipraneeth0908@users.noreply.github.com>
2024-01-28 22:43:41 -0500

    Add files via upload

---
+++ question10.py
@@ -0,0 +1,57 @@
+def question10_1():
+    answer = True
+    return answer
+
+def question10_2():
+    answer = True
+    #The correlation coefficient is a value between -1 and 1, indicating the strength and direction of a linear relationship between two sets of real values.
+    
+    return answer
+
+def question10_3():
+    answer = False
+    #Aggregating a daily time series can be more appropriate than sampling to preserve important patterns and trends in the data.
+    
+    return answer
+
+def question10_4():
+    answer = False
+    # Noise is random variability in the data, while outliers are extreme values. They are related concepts but not always the same.
+    
+    return answer
+
+def question10_5():
+    answer = False
+    #An outlier is an extreme value, but noise is random variability; not all outliers are noise.
+    
+    return answer
+
+def question10_6():
+    answer = True
+    #An asymmetric binary attribute has values 0 and 1, but it doesn't necessarily mean an equal distribution of 0s and 1s.
+    
+    return answer
+
+def question10_7():
+    answer = True
+    # If vectors of counts have a cosine measure of 1, it means the objects have the same direction and are identical.
+    
+    return answer
+
+def question10_8():
+    answer = False
+    #Discrete variables can be ratio, depending on the nature of the variable. For example, age can be a ratio variable.
+    
+    return answer
+
+def question10_9():
+    answer = False
+    #Quantitative variables can be discrete or continuous; they don't have to be continuous.
+    
+    return answer
+
+def question10_10():
+    answer = False
+    #Converting ordinal variables to asymmetric binary variables can lose information about the ordinal ranking.
+    
+    return answer
---
+++ question6.py
@@ -0,0 +1,21 @@
+def question6_1():
+    
+    answer = {
+        'equal_width': ['Change', 0], 
+        'equal_frequency': ['Change',0 ]
+    }
+    return answer
+
+def question6_2():
+    answer = {
+        'equal_width': ['Change', 2],
+        'equal_frequency': ['Change',2 ]
+    }
+    return answer
+
+def question6_3():
+    answer = {
+        'equal_width': ['Change', 2], 
+        'equal_frequency': ['Change',2 ]
+    }
+    return answer
