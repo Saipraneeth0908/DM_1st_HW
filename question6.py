1db3c2777cb3ceb232e73f13778a1aeb50283c08
Saipraneeth0908 <156871819+Saipraneeth0908@users.noreply.github.com>
2024-01-29 22:35:13 -0500

    Add files via upload

---
+++ question6.py
@@ -0,0 +1,21 @@
+def question6_1():
+    
+    answer = {
+        'equal_width': ['Change', 3], 
+        'equal_frequency': ['Change', 3]
+    }
+    return answer
+
+def question6_2():
+    answer = {
+        'equal_width': ['No Change', 7], # doubt with 5th point
+        'equal_frequency': ['No Change', 9]
+    }
+    return answer
+
+def question6_3():
+    answer = {
+        'equal_width': ['Change', 2], # doubt with 5th point
+        'equal_frequency': ['Change',9 ]
+    }
+    return answer
