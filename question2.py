59bf7d1ecbf73cad7ac407d4f5cb6ba606cebaa7
Saipraneeth0908 <156871819+Saipraneeth0908@users.noreply.github.com>
2024-01-30 00:19:46 -0500

    Add files via upload

---
+++ question2.py
@@ -0,0 +1,23 @@
+def question2_1():
+    answer = ['Discrete', 'Quantitative', 'Ratio']
+    return answer
+
+def question2_2():
+    answer = ['Continuous', 'Quantitative', 'Ratio']
+    return answer
+
+def question2_3():
+    answer = ['Continuous', 'Quantitative', 'Ratio']
+    return answer 
+
+def question2_4():
+    answer = ['Discrete', 'Quantitative', 'Ordinal']
+    return answer
+
+def question2_5():
+    answer =['discrete','qualitative', 'Nominal']
+    return answer
+
+
+
+
