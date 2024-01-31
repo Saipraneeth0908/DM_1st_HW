65c8fec7c4034405c6875108ccb7a825a11e7446
Saipraneeth0908 <156871819+Saipraneeth0908@users.noreply.github.com>
2024-01-30 20:12:26 -0500

    Add files via upload

---
+++ question7.py
@@ -0,0 +1,12 @@
+def question7_1():
+    answer = 'increase/decrease'
+    return answer
+
+def question7_2():
+    answer = 'non-increasing'
+    return answer
+
+def question7_3():
+    answer = [(0.0, 2.0), (2.0, 3.0), (3.0, 5.0), (5.0, 'infinity')]
+    return answer 
+
